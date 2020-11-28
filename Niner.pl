fc01f600dc5e5f690bae79e883b2230fa9686075
Will <will0912@icloud.com>
2020-11-28 17:59:19 +0000

    Update Niner.pl

--- Niner.pl
+++ Niner.pl
@@ -1,3 +1,4 @@
+% Animals
 animal( aardvark ).
 animal( antelope ).
 animal( coyote ).
@@ -8,6 +9,7 @@ animal( horse ).
 animal( jaguar ).
 animal( kangaroo ).
 
+% Vegetables
 vegetable( artichoke ).
 vegetable( cabbage ).
 vegetable( carrot ).
@@ -18,6 +20,7 @@ vegetable( marrow ).
 vegetable( onion ).
 vegetable( potato ).
 
+% Minerals
 mineral( anatase ).
 mineral( basalt ).
 mineral( cobolt ).
@@ -28,15 +31,33 @@ mineral( sodium ).
 mineral( silver ).
 mineral( zircon ).
 
+isWord( X, Y )
+    :- atom_length(X, XL),
+        atom_length(Y, YL),
+        writeln([XL, YL]).
+
+
+
+
+findWord( X )
+    :- forall( animal(Y), isWord(X, Y) ).
+    
+append3(X, Y, Z, Total) :-
+    append(X, YZ, Total),
+    append(Y, Z, YZ).
 
 
 
 main :-
-    MyAnimal = 452589,
-    MyVegetable = 658785,
-    MyMineral = 7378719,
-    Word = [],
-    
-
+    MyAnimal = '452589',
+    MyVegetable = '658785',
+    MyMineral = '7378719',
+    atom_chars(MyAnimal, MyAnimalList),
+    atom_chars(MyVegetable, MyVegetableList),
+    atom_chars(MyMineral, MyMineralList),
+    append3(MyAnimalList, MyVegetableList, MyMineralList, T),
+    sort(T, T1),
+    writeln( T1 ),
+    Word = [0,0,0,0,0,0,0,0,0].
     %:- animal( dingo ).
-    %:- atom_chars( test, X ), write(X).
+    %atom_chars( test, X ), write(X).
