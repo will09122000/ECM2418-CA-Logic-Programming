9efbf0ffb47a212c45323b33b0b2c8bed585a59c
Will <will0912@icloud.com>
2020-12-06 10:48:02 +0000

    Done

--- Niner.pl
+++ Niner.pl
@@ -31,48 +31,67 @@ mineral( sodium ).
 mineral( silver ).
 mineral( zircon ).
 
-spell(X, Z)
-    :- atom_chars(X, Z).
+% Spells out the word as a list of atoms.
+spell( X, Y )
+    :- atom_chars( X, Y ).
 
-findAnimal(X, Animal)
-    :- animal(X),
-        spell(X, [_,Z,_,Z,_,_]),
-        spell(X, Animal).
+% Returns an Animal that is 6 letters long and has the same letter in
+% the 2nd and 4th position of the word.
+findAnimal( X, Animal )
+    :- animal( X ),
+        spell( X, [_, Y, _, Y, _, _] ),
+        spell( X, Animal ).
 
-findVegetable(X, Vegetable)
-    :- vegetable(X),
-        spell(X, [_,_,Z,_,Z,_]),
-        spell(X, Vegetable).
+% Returns a Vegetable that is 6 letters long and has the same letter in
+% the 2nd and 5th position and the same letter in the 3rd and 6th
+% position of the word
+findVegetable( X, Vegetable )
+    :- vegetable( X ),
+        spell( X, [_, Y, Z, _, Z, Y] ),
+        spell( X, Vegetable ).
 
-findMineral(X, Mineral)
-    :- mineral(X),
-        spell(X, [Z,_,Z,_,Z,_,_]),
-        spell(X, Mineral).
+% Returns a Mineral that is 7 letters long and has the same letter in
+% the 1st, 3rd and 5th position of the word.
+findMineral( X, Mineral )
+    :- mineral( X ),
+        spell( X, [Y, _, Y, _, Y, _, _] ),
+        spell( X, Mineral ).
 
-merge([X], [Y], [[X,Y]]).
-merge([X|L1], [Y|L2], [[X,Y]|L3])
-    :- merge(L1, L2, L3).
+% Zips two lists to a list of lists.
+% Used to assign each clue number to its corresponding letter.
+zip( [X], [Y], [[X, Y]] ).
+zip( [X|List1], [Y|List2], [[X,Y]|ListZipped] )
+    :- zip( List1, List2, ListZipped ).
 
-append3(X, Y, Z, FullList)
-    :- append(X, PartialList, FullList),
-        append(Y, Z, PartialList).
+% Removes all integers from a list.
+removeInts( [], [] ).
+removeInts( [H|T], [H|T1] )
+    :- \+ integer( H ),
+        removeInts( T, T1 ).
+removeInts( [_|T], T1 )
+    :- removeInts( T, T1 ).
 
-remove_integers([],[]).
-remove_integers([X|Xs],[X|Ys]) :-
-        \+integer(X),!,
-        remove_integers(Xs,Ys).
-remove_integers([_|Xs],Ys) :-
-        remove_integers(Xs,Ys).
-
+% Finds a solution to the niner.
 main
     :- Clues = [4,5,2,5,8,9,6,5,8,7,8,5,7,3,7,8,7,1,9],
-        findAnimal(_, Animal),
-        findVegetable(_, Vegetable),
-        findMineral(_, Mineral),
-        append3(Animal, Vegetable, Mineral, X),
-        merge(X, Clues, X1),
-        sort(2, @<, X1, X2),
-        append(X2, X3),
-        remove_integers(X3, X4),
-        atomic_list_concat(X4, X5),
-        writeln(X5).
+        % Finds the correct word for each category.
+        findAnimal( _, Animal ),
+        findVegetable( _, Vegetable ),
+        findMineral( _, Mineral ),
+
+        % Each word is joint together in a list to match the Clues list.
+        append( Animal, PartialList, FullList ),
+        append( Vegetable, Mineral, PartialList ),
+
+        % Creates a list of lists.
+        zip( FullList, Clues, X ),
+
+        % Sorts the list of lists using the number and removes duplicates.
+        sort( 2, @<, X, X1 ),
+
+        % Converts the list of lists to one list.
+        append( X1, X2 ),
+
+        removeInts( X2, X3 ),
+        atomic_list_concat( X3, Solution ),
+        writeln( Solution ).
